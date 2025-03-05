f0cd40445ad40115ebecb58e8ff83fcf0f36ba58
David <david48kariuki@gmail.com>
2025-03-05 18:37:40 +0300

    complete'

---
+++ index.js
@@ -0,0 +1,33 @@
+// function foo() {
+//   return "bar";
+// } console.log(foo());
+// const foo = () => {
+//   return "bar";
+// };
+// console.log(foo());
+// const sum = 1 + 1;
+// console.log(sum);
+// const foo = function () {
+//   return "bar";
+// };
+// // console.log(foo());
+// const add = (parameter1, parameter2) => parameter1 + parameter2;
+// add(2, 3);
+// console.log(add())
+// const twoAdder = (x) => x + 2;
+// console.log(twoAdder(3))
+// const sum = (parameter1, parameter2) => {
+//   console.log(`Adding ${parameter1}`);
+//   console.log(`Adding ${parameter2}`);
+//   return parameter1 + parameter2;
+// };
+// sum(1, 2);
+
+// console.log(sum(1, 2)
+const divide = function (a, b) {
+  return a / b;
+};
+
+const square = (x) => x * x;
+
+const add = (a, b) => a + b;
